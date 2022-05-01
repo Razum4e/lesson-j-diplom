b05069d26a84870431e130c4a37dec1b8c85737d
Aleksandr Razumov <Razum.ya@ya.ru>
2022-05-01 14:07:58 +0200

    Оставил только запуск сервере, работающим, без закрытия потока сервера, который ожидает на вход слово word

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,18 @@ public class Main {
         var server = new StreamSearchServer(9999, "pdfs"); // запуск поискового сервера
         server.start();                                          // запуск параллельно работающего серверного потока
 
-        var socket = new Socket("localhost", 9999);     //открываем локальный локет
+//        var socket = new Socket("localhost", 9999);     //открываем локальный локет
+//
+//        var out = new PrintWriter(socket.getOutputStream(), true); // поток отправки в сокет
+//        out.println("бизнес");                                              // отправляем строку в поток out
+//
+//        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));// поток приема из сокета
+//        System.out.println(in.readLine());                        //печать того, что приняли с потока
+//
+//        out.close(); //закрываем поток выхода
+//        in.close(); // закрываем поток ввода
+//
+//        server.interrupt(); //закрываем поток сервера
 
-        var out = new PrintWriter(socket.getOutputStream(), true); // поток отправки в сокет
-        out.println("бизнес");                                              // отправляем строку в поток out
-
-        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));// поток приема из сокета
-        System.out.println(in.readLine());                        //печать того, что приняли с потока
-
-        out.close(); //закрываем поток выхода
-        in.close(); // закрываем поток ввода
-
-        server.interrupt(); //закрываем поток сервера
     }
 }
